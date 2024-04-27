3f3d30f004d1c41758e9d9f393464c7db599a44b
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-04-26 21:51:00 -0500

    Add import of new models

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,13 @@
+import Post from './post.js';
 import User from './user.js';
+import Comment from './comment.js';
+import Role from './role.js';
+import Permission from './permission.js';
 
 export default {
   User,
+  Post,
+  Comment,
+  Role,
+  Permission,
 };
