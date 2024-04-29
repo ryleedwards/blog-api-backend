8ea78ff4c6b7c836b52e41e2baa735efeb2aea73
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-04-29 12:56:23 -0500

    Add db connection

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
 import models from './models/index.js';
 import routes from './routes/index.js';
+import mongoose from 'mongoose';
 
 const app = express();
+
+dbConnect().catch((err) => console.log(err));
+async function dbConnect() {
+  await mongoose.connect(process.env.DB_CONNECTION);
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
