424f043c815f9b8ca0bb3ed7f85541dcf45093b0
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-04-29 12:56:58 -0500

    Change passing of user_list function

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,8 +3,6 @@ import { user_list } from '../controllers/userController.js';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  return user_list;
-});
+router.get('/', user_list);
 
 export default router;
