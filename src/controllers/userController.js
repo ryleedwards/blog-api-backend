0b3ef7bd5b834e40bbc835a9e995434d253de0e5
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-05-01 14:10:09 -0500

    Add update and delete user funcs

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -57,24 +57,83 @@ export const createUser = [
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       // There are errors. Return them to the client
+      // This pulls the first error message in validationResult errors[]
       next(new CustomError(errors.array()[0].msg, 400));
+    } else {
+      // create new user model based on params
+      const user = new User({
+        username: req.body.username,
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+      });
+
+      // TODO - password hashing
+
+      const result = await user.save();
+      res.status(201).json(result);
     }
-
-    // create new user model based on params
-    const user = new User({
-      username: req.body.username,
-      email: req.body.email,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-    });
-
-    // TODO - password hashing
-
-    const result = await user.save();
-    res.status(201).json(result);
   }),
 ];
 
 // update user
 
+export const updateUser = [
+  // Validate and sanitize fields
+  body('username')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Username must be specified')
+    .isAlphanumeric()
+    .withMessage('Username must be alphanumeric'),
+  body('email')
+    .trim()
+    .isEmail()
+    .escape()
+    .withMessage('Email must be specified')
+    .toLowerCase(),
+  body('firstName')
+    .trim()
+    .escape()
+    .isAlpha()
+    .withMessage('Please use valid characters in first name')
+    .optional(),
+  body('lastName')
+    .trim()
+    .escape()
+    .isAlpha()
+    .withMessage('Please use valid characters in last name')
+    .optional(),
+
+  // Process request after validation and sanitation
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // There are errors. Return them to the client
+      // This pulls the first error message in validationResult errors[]
+      next(new CustomError(errors.array()[0].msg, 400));
+    } else {
+      const { userId } = req.params;
+      const user = new User({
+        _id: userId,
+        username: req.body.username,
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+      });
+      const updatedUser = await User.findByIdAndUpdate(userId, user, {
+        returnDocument: 'after',
+      });
+      res.status(200).json(updatedUser);
+    }
+  }),
+];
+
 // delete user
+
+export const deleteUser = asyncHandler(async (req, res, next) => {
+  const { userId } = req.params;
+  const deletedUser = await User.findByIdAndDelete(userId);
+  res.status(200).json({ message: 'User deleted', _id: deletedUser._id });
+});
