9b6090a5d65280078e61969e9ff2b50537a38a58
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-05-01 12:59:45 -0500

    Add createUser for /users post route

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,7 @@
 import asyncHandler from 'express-async-handler';
 import { body, validationResult } from 'express-validator';
 import User from '../models/user.js';
+import CustomError from '../utils/CustomError.js';
 
 // return all users
 export const getUsers = asyncHandler(async (req, res, next) => {
@@ -14,9 +15,66 @@ export const getUsers = asyncHandler(async (req, res, next) => {
   res.json(allUsers);
 });
 
-// return specific user by ID param
+// return specific user by ID
 export const getUser = asyncHandler(async (req, res, next) => {
   const { userId } = req.params;
   const user = await User.findById(userId).exec();
   res.json(user);
 });
+
+// create user
+export const createUser = [
+  // Validate and sanitize fields
+  body('username')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Username must be specified')
+    .isAlphanumeric()
+    .withMessage('Username must be alphanumeric'),
+  body('email')
+    .trim()
+    .isEmail()
+    .escape()
+    .withMessage('Email must be specified')
+    .toLowerCase(),
+  body('firstName')
+    .trim()
+    .escape()
+    .isAlpha()
+    .withMessage('Please use valid characters in first name')
+    .optional(),
+  body('lastName')
+    .trim()
+    .escape()
+    .isAlpha()
+    .withMessage('Please use valid characters in last name')
+    .optional(),
+
+  // Process request after validation and sanitation
+  asyncHandler(async (req, res, next) => {
+    // Extract validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // There are errors. Return them to the client
+      next(new CustomError(errors.array()[0].msg, 400));
+    }
+
+    // create new user model based on params
+    const user = new User({
+      username: req.body.username,
+      email: req.body.email,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+    });
+
+    // TODO - password hashing
+
+    const result = await user.save();
+    res.status(201).json(result);
+  }),
+];
+
+// update user
+
+// delete user
