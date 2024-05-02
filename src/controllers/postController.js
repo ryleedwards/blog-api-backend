cd868f19883fc5f9bbc31a78ae30922a628d7c5e
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-05-02 10:50:05 -0500

    Add updatePost

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -62,8 +62,58 @@ export const createPost = [
 
 // update post
 
-export const updatePost = asyncHandler(async (req, res, next) => {});
+export const updatePost = [
+  // Validate and sanitize fields
+  body('title')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Title must be specified'),
+  body('author').trim().isMongoId().withMessage('Author must be specified'),
+  body('published')
+    .optional()
+    .isBoolean()
+    .withMessage('published must be boolean'),
+  body('publishedAt')
+    .optional()
+    .isISO8601()
+    .withMessage('publishedAt must be date'),
+  body('comments').optional().isArray().withMessage('comments must be array'),
+
+  // Process request after validation and sanitation
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      next(new CustomError(errors.array()[0], 400));
+    }
+    const { postId } = req.params;
+    const post = new Post({
+      _id: postId,
+      author: req.body.author,
+      title: req.body.title,
+      metaTitle: req.body.metaTitle,
+      published: req.body.published,
+      publishedAt: req.body.publishedAt,
+      content: req.body.content,
+      comments: req.body.comments,
+    });
+    const updatedPost = await Post.findByIdAndUpdate(postId, post, {
+      returnDocument: 'after',
+    });
+    if (updatedPost === null) {
+      next(new CustomError('Post not found', 404));
+    }
+    res.status(200).json(updatedPost);
+  }),
+];
 
 // delete post
 
-export const deletePost = asyncHandler(async (req, res, next) => {});
+export const deletePost = asyncHandler(async (req, res, next) => {
+  const { postId } = req.params;
+  const deletedPost = await Post.findByIdAndDelete(postId);
+  if (deletedPost === null) {
+    next(new CustomError('Post not found', 404));
+  }
+  res.status(200).json({ message: 'Post deleted', _id: deletedPost._id });
+});
