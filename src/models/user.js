f9461b690bc6e1f7761c42e437cc9cb94fe55991
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-05-01 19:24:57 -0500

    Add comparePassword function

--- src/models/user.js
+++ src/models/user.js
@@ -31,6 +31,11 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+userSchema.methods.comparePassword = async function (password) {
+  const isMatch = await bcrypt.compare(password, this.password);
+  return isMatch;
+};
+
 userSchema.plugin(mongooseUniqueValidator, { message: 'is already taken.' });
 
 const User = mongoose.model('User', userSchema);
